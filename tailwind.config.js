5959845720a6ae0bc90e2b2f6cd5506841c90475
Mohamed Adem <med.adem.1999@gmail.com>
2024-04-14 17:02:50 +0100

    Updated configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        TK: {
-          background: '#131921',
-          default: '#131921',
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/tw-elements-react/dist/js/**/*.js',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                TK: {
+                    background: '#131921',
+                    default: '#131921',
+                },
+            },
         },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };

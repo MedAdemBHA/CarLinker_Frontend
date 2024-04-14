2ef3ebcf285ec4762ecc824c522e6388a7ea7094
Mohamed Adem <med.adem.1999@gmail.com>
2024-04-14 17:06:56 +0100

    configured Query client for improver performance

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+    QueryCache,
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import App from './App.tsx';
@@ -8,7 +12,24 @@ import './index.css';
 
 import ReactDOM from 'react-dom/client';
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+            cacheTime: 120000,
+        },
+    },
+    queryCache: new QueryCache({
+        onError: (error: any, query) => {
+            // 🎉 only show error toasts if we already have data in the cache
+            // which indicates a failed background update
+            if (query.state.data !== undefined) {
+                console.error(`Something went wrong: ${error.message}`);
+            }
+        },
+    }),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
